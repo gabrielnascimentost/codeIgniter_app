389bb57612a84b15f11cf7a5adb15144f4e14d5e
Gabriel Nascimento <gabrielnascimenttto@outlook.com>
2019-05-14 11:44:21 -0300

    course in pause

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>  
-    <h3>P√°gina principal!</h3>
-</body>
-</html>
+<h3>P·gina Incial</h3>
+<p>·····„„„~~a„~~a„„„</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
+    <title><?php echo $title?></title>
+</head>
+<body>
+
+    
